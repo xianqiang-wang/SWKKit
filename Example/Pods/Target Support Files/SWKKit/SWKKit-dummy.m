dbc4bb3072472527c8a152c243d1c1ddd1dbd542
wangxianqiang <wangxianqiang@ihealthlabs.com.cn>
2020-04-10 11:52:18 +0800

    “commit

---
+++ Example/Pods/Target Support Files/SWKKit/SWKKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SWKKit : NSObject
+@end
+@implementation PodsDummy_SWKKit
+@end
---
+++ Example/Pods/Target Support Files/SWKKit/SWKKit-umbrella.h
@@ -0,0 +1,27 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "DDMessage.h"
+#import "DDMessageHeader.h"
+#import "DDMessageSlideupController.h"
+#import "DDMessageUIDelegate.h"
+#import "DDMessageUIUitls.h"
+#import "DDMessageView.h"
+#import "DDMessageViewController.h"
+#import "DDMessageWindow.h"
+#import "DDMessageWindowController.h"
+#import "DDWindowManager.h"
+#import "DDWindowUIController.h"
+
+FOUNDATION_EXPORT double SWKKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char SWKKitVersionString[];
+
